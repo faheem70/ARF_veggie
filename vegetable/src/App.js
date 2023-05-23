fbf8f9e6b71e92745111e136c7465c15c37d1c62
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-05-24 02:00:00 +0530

    initial commit

---
+++ vegetable/src/App.js
@@ -0,0 +1,35 @@
+
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import toast, { Toaster } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { setDataProduct } from './redux/productSlice';
+function App() {
+
+  const dispatch = useDispatch()
+  const productData = useSelector((state) => state.product)
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`)
+      const resData = await res.json()
+      console.log(resData);
+      dispatch(setDataProduct(resData))
+    })()
+  }, [])
+  return (
+    <>
+      <Toaster />
+      <div>
+        <Header />
+        <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
+          <Outlet />
+        </main>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ vegetable/src/redux/productSlice.js
@@ -0,0 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+
+const initialState = {
+    productList: [],
+    cartItem: [],
+}
+
+
+export const productSlice = createSlice({
+    name: "product",
+    initialState,
+    reducers: {
+        setDataProduct: (state, action) => {
+            console.log(action)
+            state.productList = [...action.payload];
+        },
+        addCartitems: (state, action) => {
+            const check = state.cartItem.some((el) => el._id === action.payload._id);
+            if (check) {
+                toast("Already Items in Cart");
+
+            } else {
+                toast("Item added successfully");
+                const total = action.payload.price;
+                state.cartItem = [
+                    ...state.cartItem,
+                    { ...action.payload, qty: 1, total: total },
+                ];
+            }
+        },
+        deleteCartItems: (state, action) => {
+            toast("One item delete successfully");
+            const index = state.cartItem.findIndex((el) => el._id === action.payload);
+            state.cartItems.splice(index, 1);
+            console.log(index);
+        },
+        increaseQty: (state, action) => {
+            const index = state.cartItem.findIndex((el) => el._id === action.payload);
+            let qty = state.cartItem[index].qty;
+            const qtyInc = ++qty;
+            state.cartItem[index].qty = qtyInc;
+
+            const price = state.cartItem[index].price;
+            const total = price * qtyInc;
+
+            state.cartItem[index].total = total;
+        },
+        decreaseQty: (state, action) => {
+            const index = state.cartItem.findIndex((el) => el._id === action.payload);
+            let qty = state.cartItem[index].qty;
+            if (qty > 1) {
+                const qtyDec = ++qty;
+                state.cartItem[index].qty = qtyDec;
+
+                const price = state.cartItem[index].price;
+                const total = price * qtyDec;
+
+                state.cartItem[index].total = total;
+            }
+        },
+    },
+
+})
+
+export const { setDataProduct, addCartitems, deleteCartItems, increaseQty, decreaseQty } = productSlice.actions
+
+export default productSlice.reducer
