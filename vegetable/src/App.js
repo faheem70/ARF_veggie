53f871e236659774f345228baabc696180651e4a
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-05-24 15:27:37 +0530

    update

--- vegetable/src/App.js
+++ vegetable/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`)
+      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}https://veggie-a.onrender.com/product`)
       const resData = await res.json()
       console.log(resData);
       dispatch(setDataProduct(resData))
